bc31d1e00d4bfe51b6b9bab7f01f1acdd8720aa5
Govardhan Reddy <90597616+govardhanreddy66@users.noreply.github.com>
2026-08-03 12:56:09 +0530

    Align committed build id with the stripped 1.2.0 Cloudflare deploy.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="rvp-youth-mscwhjcx",BUILD="mscwhjcx",BASE="";
+const CACHE="rvp-youth-mscwlk2m",BUILD="mscwlk2m",BASE="";
 async function clearAllCaches(){const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));}
 self.addEventListener("message",e=>{
   const type=e.data&&e.data.type;
